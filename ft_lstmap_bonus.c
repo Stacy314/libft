f6f5974caf22265ccccf333d8bf15d193bb5a500
Anastasiia Pechkovska <apechkov@c4r1p9.42vienna.com>
2024-09-11 11:41:36 +0200

    libft

---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apechkov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 15:50:36 by apechkov          #+#    #+#             */
+/*   Updated: 2024/04/30 17:59:00 by apechkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*buff;
+	void	*temp;
+
+	buff = (void *)0;
+	while (lst)
+	{
+		temp = f(lst->content);
+		new = ft_lstnew(temp);
+		if (!new)
+		{
+			ft_lstclear(&buff, del);
+			return (free(temp), buff);
+		}
+		ft_lstadd_back(&buff, new);
+		lst = lst->next;
+	}
+	return (buff);
+}
---
+++ ft_putstr_fd.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apechkov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 16:34:04 by apechkov          #+#    #+#             */
+/*   Updated: 2024/04/15 16:34:05 by apechkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
+/*int	main(void)
+{
+	char *my_string = "Hello, World!"; 
+	int fd = 2;
+	ft_putstr_fd(my_string, fd);
+	return(0);
+}*/
